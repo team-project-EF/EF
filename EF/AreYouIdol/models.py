3cd26153c314fca83a752688141a6793d7476a13
Quedo999 <sri00119@naver.com>
2021-12-15 07:21:03 +0900

    # 업로드 이미지 미리보기, 폴더 사진 저장

--- EF/AreYouIdol/models.py
+++ EF/AreYouIdol/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields import BLANK_CHOICE_DASH
 
 # Create your models here.
+
+# 미디어 파일의 세부 정보를 저장하기 위한 class 지정
+class Upload(models.Model):
+    img = models.ImageField(upload_to='images/', blank=True, null=True)
--- EF/AreYouIdol/views.py
+++ EF/AreYouIdol/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from AreYouIdol.models import Upload
 
 # Create your views here.
+def find(request):
+    if request.method == 'POST':
+        print(request.POST)
+        img = request.FILES["img"]
+
+        upload = Upload()
+        upload.img = img
+        upload.save()
+
+        # 이미지 url 넘겨주기
+        # context = {
+        #     'img' : upload.img
+        #  }
+
+        messages.success(request, upload.img.url)
+        return redirect('/')
+    else:
+        return render(request, 'areyouidol.html')
