63147b2304864feca4f6e22dab33c49549214b87
Quedo999 <sri00119@naver.com>
2021-12-15 11:32:45 +0900

    # 이미지 업로드 시 크롭

--- EF/AreYouIdol/views.py
+++ EF/AreYouIdol/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from AreYouIdol.models import Upload
+from .PreProcessing.align_faces import crop
 
 # Create your views here.
+
 def find(request):
     if request.method == 'POST':
         print(request.POST)
@@ -12,12 +14,15 @@ def find(request):
         upload.img = img
         upload.save()
 
+        crop()
+
         # 이미지 url 넘겨주기
         # context = {
         #     'img' : upload.img
         #  }
 
         messages.success(request, upload.img.url)
+        
         return redirect('/')
     else:
         return render(request, 'areyouidol.html')
